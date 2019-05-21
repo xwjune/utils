edce19ef2db544c3c6b031e36e2e4cd91c2d9bfa
xwjune <xwjune@163.com>
2019-05-21 10:50:52 +0800

    webpack配置优化

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig, {
+  devtool: 'none',
   mode: 'development',
   output: {
     filename: 'jun-utils.js',
