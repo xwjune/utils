923bbf9ac098f99e2855995c500ac219db4d519c
xwjune <xwjune@163.com>
2026-05-20 15:52:25 +0800

    fix: 优化邮箱校验正则

--- src/check/email.js
+++ src/check/email.js
@@ -2,6 +2,11 @@
  * 邮箱校验
  * 规则：登录名@主机名.域名
  *
+ * ^[a-zA-Z0-9._-]+：表示邮箱名的开始，可以包含字母、数字、点（.）、下划线（_）或破折号（-），并且至少有一个字符。
+ * @：邮箱地址必须包含一个@符号。
+ * [a-zA-Z0-9.-]+：表示域名部分，可以包含字母、数字、点（.）或破折号（-），并且至少有一个字符。
+ * \.[a-zA-Z]{2,6}$：表示顶级域名（TLD），以点（.）开始，后面跟着2到6个字母。
+ *
  * @param {*} value - The value to check.
  * @return {Boolean} Return `true` if validated, else `false`.
  *
@@ -12,5 +17,5 @@
  * // => true
  */
 export default function email(value) {
-  return /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z_-]+(\.[0-9a-zA-Z_-]+)+$/.test(value);
+  return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(value);
 }
